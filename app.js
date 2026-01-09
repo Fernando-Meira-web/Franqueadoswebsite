0351c6a4f8de6d488a00b5ed0fc0846788b2c379
Fernando-Meira-web <fmeira218@gmail.com>
2026-01-09 13:23:42 -0400

    Fix Render PORT and gateway error

--- app.js
+++ app.js
@@ -138,8 +138,9 @@ app.get("/health", (req, res) => {
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log("Servidor rodando na porta " + PORT);
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
+
 
 app.post("/enviar", (req, res) => {
   req.url = "/enpostar";
